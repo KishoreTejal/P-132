6740d4e2da7dbaa4778d2db8806ee33a312badea
KishoreTejal <87982146+KishoreTejal@users.noreply.github.com>
2023-08-11 16:57:30 +0530

    Add files via upload

---
+++ TV.js
@@ -0,0 +1,46 @@
+Status = "";
+TV_image = "";
+objects = [];
+
+function preload(){
+    TV_image = loadImage("TV.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    objectDetector.detect(TV_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    if (Status != undefined) {
+        image(TV_image , 0 , 0 , 640 , 420);
+        for (var i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill(255,0,0);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x + 5 , objects[i].y + 15);
+            noFill();
+            stroke(255,0,0);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+
+}
